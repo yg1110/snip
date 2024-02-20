88b1945f879d0e3e33a0453dda260d4b40872529
yg1110 <younggil9488@gmail.com>
2024-02-20 23:52:01 +0900

    front 코드 추가

---
+++ front/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "snip",
+  description: "snip",
+  viewport:
+    "width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
